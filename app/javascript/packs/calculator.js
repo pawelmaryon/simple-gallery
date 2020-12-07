2304edd64726810ffe0c227de00e9c875744114e
Pawel Maryon <maryonpawel@gmail.com>
2020-12-07 12:37:10 +0000

    calculator view outline

--- app/javascript/packs/calculator.js
+++ app/javascript/packs/calculator.js
@@ -1,4 +1,12 @@
-const materialType = document.querySelector(.material_name);
-materialType.addEventListener("click", function () {
-    console.log("I have clicked");
-} )
+const materialTypeBtn = document.querySelector('.material-name')
+const dropdownContent = document.querySelector('.dropdown');
+const deckingItems = document.querySelector('.decking-items');
+const deckingType = document.querySelector('.decking-type');
+dropdownContent.classList.add('hidden');
+
+materialTypeBtn.addEventListener('click' , function () {
+   dropdownContent.classList.remove('hidden')
+   deckingType.addEventListener('click', function() {
+    deckingItems.classList.remove('hidden')
+})
+});
