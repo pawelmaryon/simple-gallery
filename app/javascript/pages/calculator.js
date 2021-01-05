d2ad607f781a204cc6a396cc108f724f4d3775ea
Pawel Maryon <maryonpawel@gmail.com>
2021-01-05 21:45:15 +0000

    material buttons implemented

--- app/javascript/pages/calculator.js
+++ app/javascript/pages/calculator.js
@@ -1,5 +1,5 @@
-const materialName = document.querySelector('.material-name')
-
+const materialName = document.querySelector('.material-name');
+const materialList = documant.querySelector('.material-list');
 materialName.addEventListener('click', function() {
   
 })
