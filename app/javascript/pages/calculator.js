890752637af6826503903455944f22d3a781eac4
Pawel Maryon <maryonpawel@gmail.com>
2021-01-19 22:15:39 +0000

    list items for materials created

--- app/javascript/pages/calculator.js
+++ app/javascript/pages/calculator.js
@@ -1,7 +1,9 @@
 let materialName = document.querySelector('.material-name');
 let materialList = document.querySelector('.material-list');
+
 materialName.addEventListener('click', function() 
 {
+
   console.log('button clicked');
   materialList.classList.toggle('hidden');
 });
