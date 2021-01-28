87616d2bf0884bce3347ba56eef4e76bdbef04d1
Pawel Maryon <maryonpawel@gmail.com>
2021-01-28 21:43:23 +0000

    new calculator form implemented

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,7 +1,18 @@
 class CalculatorsController < ApplicationController
   def new
+    @calculator_item = Material.new
   end
 
   def create
+    @calculator_item = Material.new(material_params)
+    if @calculator_item.save
+      redirect_to pages_calculator_path
+    else
+      render :new
+    end
+  end
+  private
+  def material_params
+    params.require(:material_item).premit(:name, :lenght, :width, :height, :price)
   end
 end
