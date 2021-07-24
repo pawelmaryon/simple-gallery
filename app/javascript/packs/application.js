9cd18ad754a13b7fe03b1a2ba32dff38edf19036
P Maryon <maryonpawel@gmail.com>
2021-07-24 14:03:05 +0100

    js fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,25 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("pages/calculator")
 
 import "bootstrap";
 
+console.log('this is working');
+
+const materialName = document.querySelector('.material-name');
+const materialList = document.querySelector('.material-list');
+const materialItem = document.querySelector('btn-item');
+const lengthInput = document.querySelector('length');
+
+
+materialName.addEventListener('click', function() 
+{
+  console.log('button clicked');
+  materialList.classList.toggle('hidden');
+});
+// if materialItem === materialItem.textContent('decking') {
+// lengthInput.value = 120;
+// } else if materialItem === materialItem.textContent('patio') {
+//   lengthInput.innerText = 100
+// }
+// end
